e42009696b60d40392b8ba2fee0841266e4837f6
TomiwaJoseph <tomiwajoseph88@gmail.com>
2021-03-25 11:51:57 +0100

    created date frame and results frame

--- age_calc.py
+++ age_calc.py
@@ -1,6 +1,8 @@
 from tkinter import *
 from tkcalendar import DateEntry
 from datetime import datetime
+from tkinter.scrolledtext import ScrolledText
+
 
 root = Tk
 
@@ -41,33 +43,38 @@ class AgeCalculator(Frame):
         Frame.configure(self,bg='#222')
         self.root = root
         self.root.title('Intro Page')
-        self.root.geometry('650x330')
+        self.root.geometry('580x330')
         self.root.resizable(0,0)
 
         Label(text='Input',bg='#222',fg='#fff',font='montserrat 16').place(x=30,y=20)
+        Label(text='Results',bg='#222',fg='#fff',font='montserrat 16').place(x=450,y=20)#280
 
-        self.main_frame = Frame(borderwidth=2,relief='ridge',width=300,height=245,bg='#fff')
+        self.main_frame = Frame(borderwidth=2,relief='ridge',width=220,height=245,bg='#fff')
         self.main_frame.place(x=20,y=60)
-        Label(self.main_frame,text='Date of Birth:',bg='#fff',fg='teal',font='montserrat 12').place(x=10,y=20)
+        Label(self.main_frame,text='Date of Birth:',bg='#fff',fg='teal',font='montserrat 12').place(x=30,y=20)
         self.dob_date = DateEntry(self.main_frame,width=12, background='teal',
                 font='montserrat 12',foreground='white',borderwidth=5)
-        self.dob_date.place(x=10,y=50)
+        self.dob_date.place(x=30,y=50)
 
-        Label(self.main_frame,text='Target Date:',bg='#fff',fg='teal',font='montserrat 12').place(x=10,y=90)
+        Label(self.main_frame,text='Target Date:',bg='#fff',fg='teal',font='montserrat 12').place(x=30,y=90)
         self.target_date = DateEntry(self.main_frame,width=12, background='teal',
                 font='montserrat 12',foreground='white',borderwidth=5)
-        self.target_date.place(x=10,y=120)
+        self.target_date.place(x=30,y=120)
 
-        Button(self.main_frame,text='Calculate',fg='#fff',bg='teal',width=8,bd=0,
-            font='montserrat 14',command=self.calculate_it).place(x=10,y=160)
+        Button(self.main_frame,text='Calculate',fg='#fff',bg='teal',width=13,bd=0,
+            font='montserrat 14',command=self.calculate_it).place(x=27,y=160)
 
-        self.result_frame = Frame(borderwidth=2,relief='ridge',width=300,height=250,bg='#fff')
-        self.result_frame.place(x=350,y=60)
-        # Label(self.main_frame,text='Age Calculator').place(x=10, y=30)
+        self.result_frame = Frame(borderwidth=2,relief='ridge',width=300,height=245,bg='#fff')
+        self.result_frame.place(x=260,y=60)
+
+        self.results = Text(self.result_frame,fg='#fff',bg='#444',font='montserrat 14',
+            width=22,height=9)
+        self.results.place(x=3,y=2)
 
     def calculate_it(self):
         dob_date = self.dob_date.get_date()
         target_date = self.target_date.get_date()
+        self.results.insert(END, 'something')
 
 if __name__ == '__main__':
     app = Switch()
